1e2fc6b0855f07a3a4afbc1a38ea7828e6169515
Seunghyo Chun <seunghyo.chun@gmail.com>
2023-01-14 18:44:04 +0900

    add codes

--- Hello.java
+++ Hello.java
@@ -1,6 +1,35 @@
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Flux;
 
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello my java");
+
+		Flux<Integer> flux = Flux.just(1, 2, 3);
+
+		flux.subscribe(new Subscriber<>() {
+			@Override
+			public void onSubscribe(Subscription subscription) {
+				System.out.println("onSubscribe");
+				subscription.request(Integer.MAX_VALUE);
+			}
+
+			@Override
+			public void onNext(Integer integer) {
+				System.out.println("onNext :" + integer);
+			}
+
+			@Override
+			public void onError(Throwable throwable) {
+				System.out.println("onError :" + throwable);
+			}
+
+			@Override
+			public void onComplete() {
+				System.out.println("onComplete");
+
+			}
+		});
 	}
 }
